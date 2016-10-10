9107e73d173abc16944d151066cffd95f31b840e
Cory Pickrel <cpickrel4@gmail.com>
2016-10-09 20:57:29 -0500

    Add del package. Add clean-dist-concat Gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require('gulp'),
     gp_concat = require('gulp-concat'),
     gp_rename = require('gulp-rename'),
-    gp_uglify = require('gulp-uglify');
+    gp_uglify = require('gulp-uglify'),
+    del = require('del');
 
-gulp.task('minicat', function(){
+gulp.task('minicat', function() {
     return gulp.src(['js/models.js', 'js/menus.js', 'js/levels.js', 'js/resources.js', 'js/app.js', 'js/engine.js'])
         .pipe(gp_concat('concat.js'))
         .pipe(gulp.dest('dist'))
-        .pipe(gp_rename('uglify.js'))
+        .pipe(gp_rename('app.min.js'))
         .pipe(gp_uglify())
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['minicat'], function(){});
+gulp.task('clean-dist-concat', function() {
+    return del([
+        'dist/concat.js'
+    ]);
+});
+
+gulp.task('default', ['minicat', 'clean-dist-concat'], function(){});
