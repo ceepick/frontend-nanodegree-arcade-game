3f6d69977ba14c015ccefccf051c8914b6376e2c
Cory Pickrel <cpickrel4@gmail.com>
2016-10-09 20:46:12 -0500

    Add Gulp concat-minify pipeline.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    gp_concat = require('gulp-concat'),
+    gp_rename = require('gulp-rename'),
+    gp_uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
-	// TODO
+gulp.task('minicat', function(){
+    return gulp.src(['js/models.js', 'js/menus.js', 'js/levels.js', 'js/resources.js', 'js/app.js', 'js/engine.js'])
+        .pipe(gp_concat('concat.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(gp_rename('uglify.js'))
+        .pipe(gp_uglify())
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', ['minicat'], function(){});
