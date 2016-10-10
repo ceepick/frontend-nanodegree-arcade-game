021d463fe04ea4c4aa44d2640e184484b782f515
Cory Pickrel <cpickrel4@gmail.com>
2016-10-09 21:01:43 -0500

    Add run-sequence npm package. Use for gulp task order.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp'),
     gp_concat = require('gulp-concat'),
     gp_rename = require('gulp-rename'),
     gp_uglify = require('gulp-uglify'),
-    del = require('del');
+    del = require('del'),
+    runSequence = require('run-sequence');
 
+// Concat files | Minify
 gulp.task('minicat', function() {
     return gulp.src(['js/models.js', 'js/menus.js', 'js/levels.js', 'js/resources.js', 'js/app.js', 'js/engine.js'])
         .pipe(gp_concat('concat.js'))
@@ -13,10 +15,19 @@ gulp.task('minicat', function() {
         .pipe(gulp.dest('dist'));
 });
 
+// Remove concat.js after minicat runs
 gulp.task('clean-dist-concat', function() {
     return del([
         'dist/concat.js'
     ]);
 });
 
-gulp.task('default', ['minicat', 'clean-dist-concat'], function(){});
+// Run tasks sequentially
+gulp.task('tasks', function(done) {
+    runSequence('minicat', 'clean-dist-concat', function() {
+        done();
+    });
+});
+
+
+gulp.task('default', ['tasks'], function(){});
